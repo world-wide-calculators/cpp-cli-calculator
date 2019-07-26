1aaa1d93950e6c796b8c8913bdedabe5358ef16a
BM835 <devdev@bm835.space>
2019-07-26 23:43:30 +1000

    now on division by 0 answer now writing

--- main.cpp
+++ main.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 int main() 
 {
-	char retry; 
-	do{
+  char retry; 
+  do{
     char op;
     int n1, n2, sum;
+    int print_ans = 1;
     cout << "input first number: ";
     cin >> n1;
     cout << "input math sign: ";
@@ -26,13 +27,20 @@ int main()
           sum = n1 * n2;
           break;
       case '/':
-          sum = n1 / n2;
+          if(n2 == 0){
+            cout << "You can't divide by 0";
+            print_ans = 0;
+          }else{
+            sum = n1 / n2;
+          }
           break;
       case '^':
           sum = pow(n1, n2);
           break;
     }
-    cout << n1 << op << n2 << '=' << sum;
+    if(print_ans == 1){
+      cout << n1 << op << n2 << '=' << sum;
+    }
     cout << "\n Retry?(y/n)";
     cin >> retry;
     }while(retry == 'y');
